cb274d8793958a1accfb9b8d241c4b71c2ada695
cwpaterick <cwpaterick@gmail.com>
2023-04-03 23:42:06 -0500

    Week 2 Assignment

--- models/promotion.js
+++ models/promotion.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
 require('mongoose-currency').loadType(mongoose);
 
 const Currency = mongoose.Types.Currency;
 
-const promotionSchema = new mongoose.Schema({
+const promotionSchema = new Schema({
     name: {
         type: String,
         required: true,
